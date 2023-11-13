b4453a7350316a0fcc21577a3a91ab6c8e75d2ac
Sebastian <seb154@live.com>
2023-11-13 18:23:08 -0500

    modifies index.html and main.js

--- main.js
+++ main.js
@@ -2,7 +2,30 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function () {
+  const heart = document.getElementById("heart");
+  const errorModal = document.getElementById("errorModal");
+  const errorMessageElement = document.getElementById("errorMessage");
+
+  heart.addEventListener("click", function () {
+    mimicServerCall()
+      .then(() => {
+        // Simulated successful response
+        heart.classList.add("activated-heart");
+        // Add logic to update the UI for a successful response
+      })
+      .catch((error) => {
+        // Simulated error response
+        errorMessageElement.textContent = error;
+        errorModal.classList.remove("hidden");
+
+        // Hide the modal after 3 seconds
+        setTimeout.hidden(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
 
 
 
